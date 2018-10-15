04c61c07e5b1db4df69970c073665aa6581a18ea
TurtleKing94 <fabianbleck@yahoo.de>
2018-10-15 19:08:59 +0200

    initial commit
    alles wie in reli vorbereitet

---
+++ Assets/camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour {
+
+    public Camera mainCam;
+    public float scaler = 1f;
+
+	// Update is called once per frame
+	void Update () {
+        float x = mainCam.transform.position.x;
+        x -= ( Input.GetAxis("Horizontal") * scaler);
+        if(x > 15)
+        {
+            x = 15;
+        }
+        if( x < -45)
+        {
+            x = -45;
+        }
+        mainCam.transform.position = new Vector3(x, mainCam.transform.position.y, mainCam.transform.position.z);
+	}
+}
---
+++ Assets/daytime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class daytime : MonoBehaviour {
+    public DirectionalLight x;
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
