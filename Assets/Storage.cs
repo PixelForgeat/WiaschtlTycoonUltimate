45a33c9b0c28d111a5efc21487d813686e8ad677
TurtleKing94 <fabianbleck@yahoo.de>
2018-10-15 21:31:21 +0200

    0.0.1
    MainUI started(MoneyText+Backend)
    Static Dataclass

---
+++ Assets/Storage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Storage : MonoBehaviour {
+    public static float Money = 0f;
+    public static List<Product> products;
+}
+
+public class Product
+{
+    public string Name;
+    public int duration;
+}
