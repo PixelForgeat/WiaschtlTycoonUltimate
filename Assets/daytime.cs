4cd4369e9cac7e22350ee0eef2c340737bb37f0c
TurtleKing94 <fabianbleck@yahoo.de>
2018-10-16 19:24:16 +0200

    0.0.4
    - Day und Night Cykle
    - Convertierung zu neuster Unity Version

--- Assets/daytime.cs
+++ Assets/daytime.cs
@@ -1,12 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.GlobalIllumination;
 
 public class daytime : MonoBehaviour {
-    public DirectionalLight x;
 	// Update is called once per frame
 	void Update () {
-		
+        transform.Rotate(Vector3.right, Time.deltaTime * 10f);
 	}
 }
