7acc92b18ec71eff3829e3a7a6e0943339b9c8dc
TurtleKing94 <fabianbleck@yahoo.de>
2018-10-16 09:51:26 +0200

    0.0.2
    UNGLAUBLICHER MONEYUPDATER EINZEILER!!

--- Assets/UIUpdater.cs
+++ Assets/UIUpdater.cs
@@ -8,8 +8,17 @@ public class UIUpdater : MonoBehaviour {
     
 	void Update ()
     {
-        updateMoney();
 	}
+    private void LateUpdate()
+    {
+        updateMoney();
+    }
 
-    void updateMoney() => MoneyText.text = "Money " + Storage.Money.ToString();
+    public void MoneyPlusHundret()
+    {
+        Storage.Money += Random.Range(1f, 100f);
+        print(Storage.Money);
+    }
+
+    void updateMoney() => MoneyText.text = (((int)+((Storage.Money - ((int)Storage.Money)) * 100)) >= 10) ? "Money " + ((int)Storage.Money).ToString() + "," + ((int)((Storage.Money - ((int)Storage.Money)) * 100)).ToString() : "Money " + ((int)Storage.Money).ToString() + "," + "0" + ((int)((Storage.Money - ((int)Storage.Money)) * 100));
 }
