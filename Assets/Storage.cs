e596680a5e44e85338f8049b4e420d3135d0d8be
TurtleKing94 <fabianbleck@yahoo.de>
2018-10-16 10:54:32 +0200

    0.0.3
    EINZEILER HOTFIX

--- Assets/Storage.cs
+++ Assets/Storage.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Storage : MonoBehaviour {
-    public static float Money = 0f;
+    public static double Money = 0f;
     public static List<Product> products;
 }
 
 public class Product
 {
-    public string Name;
-    public int duration;
+    public Product( string sName , int iDuration )
+    {
+        m_sName = sName;
+        m_iDuration = iDuration;
+    }
+    public string m_sName;
+    public int m_iDuration;
 }
--- Assets/UIUpdater.cs
+++ Assets/UIUpdater.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class UIUpdater : MonoBehaviour {
+
     public Text MoneyText;
-    
-	void Update ()
+
+    void Update ()
     {
-	}
+        Storage.Money += Random.Range(.1f, 19999999f);
+    }
     private void LateUpdate()
     {
         updateMoney();
@@ -20,5 +23,5 @@ public class UIUpdater : MonoBehaviour {
         print(Storage.Money);
     }
 
-    void updateMoney() => MoneyText.text = (((int)+((Storage.Money - ((int)Storage.Money)) * 100)) >= 10) ? "Money " + ((int)Storage.Money).ToString() + "," + ((int)((Storage.Money - ((int)Storage.Money)) * 100)).ToString() : "Money " + ((int)Storage.Money).ToString() + "," + "0" + ((int)((Storage.Money - ((int)Storage.Money)) * 100));
+    void updateMoney() => MoneyText.text = (((long)+((Storage.Money - ((long)Storage.Money)) * 100)) >= 10) ? "Money " + ((long)Storage.Money).ToString() + "," + ((long)((Storage.Money - ((long)Storage.Money)) * 100)).ToString()+ " DEM" : "Money " + ((long)Storage.Money).ToString() + "," + "0" + ((long)((Storage.Money - ((long)Storage.Money)) * 100))+ " DEM";
 }
