f67db4981b66f3571abb4188032826f94a32e11d
TurtleKing94 <fabianbleck@yahoo.de>
2018-10-18 10:11:57 +0200

    0.0.6
    -Day Night Cykle improvment

--- Assets/Scripts/daytime.cs
+++ Assets/Scripts/daytime.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class daytime : MonoBehaviour {
-	// Update is called once per frame
-	void Update () {
-        transform.Rotate(Vector3.right, Time.deltaTime * 10f);
-	}
+    // Update is called once per frame
+    public float TurnSpeedX = 5f;
+    public float TurnSpeedZ = 5f;
+    void Update () {
+        transform.Rotate(Time.deltaTime * TurnSpeedX, 0f, Time.deltaTime * TurnSpeedZ);
+    }
 }
